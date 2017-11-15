a5ad22b8795563ee34e0e430664bfaf049fe950f
Edward Choi <edwardchoi.2011@gmail.com>
2017-11-14 18:15:35 -0800

    CRUD public wikis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111211811) do
+ActiveRecord::Schema.define(version: 20171114074831) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 20171111211811) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wikis", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "private"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wikis_on_user_id"
   end
 
 end
